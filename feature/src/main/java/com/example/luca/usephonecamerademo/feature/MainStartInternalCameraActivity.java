5e13b2e7b69bda2b91960b40a5704fc24d3f088e
Luca Spalierno <luca.spalierno@systematic.com>
2018-07-16 11:57:46 +0200

    [(master)] Cleanup. Handle rotation.

--- feature/src/main/java/com/example/luca/usephonecamerademo/feature/MainStartInternalCameraActivity.java
+++ feature/src/main/java/com/example/luca/usephonecamerademo/feature/MainStartInternalCameraActivity.java
@@ -8,7 +8,6 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,6 +29,12 @@ public class MainStartInternalCameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            lastPicturePath = savedInstanceState.getString("lastPicturePath");
+            Toast.makeText(this, "lastPicturePath: " + lastPicturePath, Toast.LENGTH_SHORT).show();
+        }
+
         setContentView(R.layout.activity_main_start_internal_camera);
 
         storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
@@ -38,17 +43,14 @@ public class MainStartInternalCameraActivity extends AppCompatActivity {
         final Button openCameraBtn = findViewById(R.id.open_camera_btn);
         openCameraBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
                 dispatchTakePictureIntent();
             }
         });
 
         final PackageManager pm = this.getPackageManager();
-
         final Button checkHasCameraBtn = findViewById(R.id.check_camera_btn);
         checkHasCameraBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
                 Toast.makeText(MainStartInternalCameraActivity.this, Boolean.toString(pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)), Toast.LENGTH_SHORT).show();
             }
         });
@@ -56,7 +58,6 @@ public class MainStartInternalCameraActivity extends AppCompatActivity {
         final Button deleteAllBtn = findViewById(R.id.delete_all);
         deleteAllBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
                 boolean allDeleted = deleteAllPics();
                 Toast.makeText(MainStartInternalCameraActivity.this, "All pictures deleted: " + Boolean.toString(allDeleted), Toast.LENGTH_SHORT).show();
             }
@@ -65,16 +66,24 @@ public class MainStartInternalCameraActivity extends AppCompatActivity {
         final Button deleteLastBtn = findViewById(R.id.delete_last_pic);
         deleteLastBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Code here executes on main thread after user presses button
                 boolean allDeleted = deleteLastPictureFile(lastPicturePath);
+                lastPictureNameView.setText(getString(R.string.last_pic_name));
                 Toast.makeText(MainStartInternalCameraActivity.this, "Last picture deleted: " + Boolean.toString(allDeleted), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        if (lastPicturePath != null) {
+            outState.putString("lastPicturePath", lastPicturePath);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String newText = lastPictureNameView.getText().toString() + lastPicturePath;
+        String newText = getString(R.string.last_pic_name) + lastPicturePath;
         lastPictureNameView.setText(newText);
 
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
@@ -112,7 +121,7 @@ public class MainStartInternalCameraActivity extends AppCompatActivity {
     }
 
 
-    String mCurrentPhotoPath;
+    String currentPhotoPath;
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -126,7 +135,7 @@ public class MainStartInternalCameraActivity extends AppCompatActivity {
         );
 
         // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
+        currentPhotoPath = image.getAbsolutePath();
         return image;
     }
 
@@ -142,7 +151,7 @@ public class MainStartInternalCameraActivity extends AppCompatActivity {
 
     private boolean deleteLastPictureFile(String path) {
         File lastPictureFile = new File(path);
-        boolean deleted = true;
+        boolean deleted = false;
 
         if (lastPictureFile.isFile() && !lastPictureFile.isDirectory()) {
             deleted = lastPictureFile.delete();
